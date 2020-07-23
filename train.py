cd7a794a871c413e5a8af7c69b13f3cd863b0fdd
Frederik Wegner <onlyknoppsa@gmail.com>
2020-07-23 12:21:20 +0200

    Step learnrate scheduler.

--- train.py
+++ train.py
@@ -15,15 +15,16 @@ def fit(net, criterion, dataset, epochs=3, batch_size=24, device="cpu"):
 	loader_test = torch.utils.data.DataLoader(ds_val, batch_size=batch_size, shuffle=False)
 
 	first_batch = next(iter(loader_test))
-	writer.add_images("validation_in_high", first_batch[0][:, 0:1])
-	writer.add_images("validation_in_high", first_batch[0][:, 1:2])
-	writer.add_images("validation_gt_high", first_batch[1][:, 0:1])
-	writer.add_images("validation_gt_high", first_batch[1][:, 1:2])
+	n = max(len(first_batch), 8)
+	writer.add_images("validation_in_high", first_batch[0][:n, 0:1])
+	# writer.add_images("validation_in_low", first_batch[0][:n, 1:2])
+	writer.add_images("validation_gt_high", first_batch[1][:n, 0:1])
+	# writer.add_images("validation_gt_low", first_batch[1][:n, 1:2])
 	
 	net.to(device)
 
 	optimizer = torch.optim.Adam(net.parameters(), lr=0.001)
-	scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 1, gamma=0.3)
+	scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=1, cooldown=2)
 
 	for e in range(epochs):
 		bar = progress.Bar("Epoch {}, train".format(e), finish=len(ds_train))
@@ -37,21 +38,23 @@ def fit(net, criterion, dataset, epochs=3, batch_size=24, device="cpu"):
 			optimizer.step()
 			writer.add_scalar('Loss/train', loss.item())
 			bar.inc_progress(len(X))
-		scheduler.step()
 
 		bar = progress.Bar("Epoch {}, test".format(e), finish=len(ds_val))
 		net.eval()
 		losses = 0
 		n_losses = 0
 		first = True
+		
 		for X, Y in loader_test:
 			X, Y = X.to(device), Y.to(device)
 			Y_ = net(X)
 			if first:
 				first = False
-				writer.add_images("validation_prediction_high", Y_[:, 0:1], global_step=e)
-				writer.add_images("validation_prediction_low", Y_[:, 1:2], global_step=e)
+				writer.add_images("validation_prediction_high", Y_[:n, 0:1], global_step=e)
+				writer.add_images("validation_prediction_low", Y_[:n, 1:2], global_step=e)
 			losses = losses + criterion(Y, Y_).item()
 			n_losses += 1
 			bar.inc_progress(len(X))
+		loss = losses / n_losses
 		writer.add_scalar('Loss/val', losses / n_losses)
+		scheduler.step(loss)
