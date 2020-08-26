c6ab1a323cfb9c9ab83eba805f6f23cec47d6b91
Jan <mail@janemrich.de>
2020-08-26 11:36:17 +0200

    denoising config improved

--- n2s-sharp.py
+++ n2s-sharp.py
@@ -41,69 +41,72 @@ def cli():
 	return parser.parse_args()	
 
 
-def fit(net, loss_function, dataset, epochs, batch_size=32):
+def fit(net, loss_function, dataset, epochs, batch_size=32, device='cpu'):
 
-    train_size = int(0.8 * len(dataset))
-    test_size = len(dataset) - train_size
-    train_dataset, test_dataset = torch.utils.data.random_split(dataset, [train_size, test_size])
+	train_size = int(0.8 * len(dataset))
+	test_size = len(dataset) - train_size
+	train_dataset, test_dataset = torch.utils.data.random_split(dataset, [train_size, test_size])
 
-    masker = Masker(width = 4, mode='interpolate')
-    optimizer = Adam(net.parameters(), lr=0.001)
+	masker = Masker(width = 4, mode='interpolate')
+	optimizer = Adam(net.parameters(), lr=0.001)
 
-    dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+	dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+
+	net.to(device)
+	loss_function.to(device)
 
 
-    # training
-    for e in range(epochs):
-        bar = progress.Bar("Epoch {}, train".format(e), finish=train_size)
-        net.train()
-        for i, batch in enumerate(dataloader):
-            noisy_images = batch
-            noisy_images = noisy_images.float()
+	# training
+	for e in range(epochs):
+		bar = progress.Bar("Epoch {}, train".format(e), finish=train_size)
+		net.train()
+		for i, batch in enumerate(dataloader):
+			noisy_images = batch.to(device)
+			noisy_images = noisy_images.float()
 
-            # for now only low input
-            noisy_images = noisy_images[:,:1,::]
-            net_input, mask = masker.mask(noisy_images, i)
-            net_output = net(net_input)
+			# for now only low input
+			noisy_images = noisy_images[:,:1,::]
+			net_input, mask = masker.mask(noisy_images, i)
+			net_output = net(net_input)
 
-            loss = loss_function(net_output*mask, noisy_images*mask)
+			loss = loss_function(net_output*mask, noisy_images*mask)
 
-            optimizer.zero_grad()
+			optimizer.zero_grad()
 
-            loss.backward()
+			loss.backward()
 
-            optimizer.step()
-            bar.inc_progress(batch_size)
+			optimizer.step()
+			bar.inc_progress(batch_size)
 
-        
-        print('\nLoss (', i, ' \t', round(loss.item(), 6))
+		
+		print('\nLoss (', i, ' \t', round(loss.item(), 6))
    
-        test_data_loader = DataLoader(test_dataset,
-                                        batch_size=32,
-                                        shuffle=False,
-                                    num_workers=3)
+		test_data_loader = DataLoader(test_dataset,
+										batch_size=32,
+										shuffle=False,
+									num_workers=3)
 
-        net.eval()
-        plot_val(net, test_data_loader)
+		net.eval()
+		plot_val(net, test_data_loader)
 
 def plot_val(net, data_loader):
-    i, test_batch = next(enumerate(data_loader))
-    noisy = test_batch.float()
-    denoised = net(noisy[:,1:,::]).detach()
-    noisy = noisy[:,1:,::]
-    comp = np.concatenate([noisy, denoised], axis=-2)
+	i, test_batch = next(enumerate(data_loader))
+	noisy = test_batch.float()
+	denoised = net(noisy[:,1:,::]).detach()
+	noisy = noisy[:,1:,::]
+	comp = np.concatenate([noisy, denoised], axis=-2)
 
-    n_pics = 5
-    fig = plt.figure(figsize=(3*n_pics, 8))
-    fig.suptitle('channel low, noisy(top) vs denoised(bottom)', fontsize=30)
-    for j in range(n_pics):
-        ax1 = fig.add_subplot(1,n_pics,j+1)
-        ax1.get_xaxis().set_visible(False)
-        ax1.get_yaxis().set_visible(False)
-        ax1.set_title("image {}".format(j))
-        ax1.imshow(comp[j][0], interpolation=None, vmin=0.0, vmax=1.0, cmap='gray')
+	n_pics = 5
+	fig = plt.figure(figsize=(3*n_pics, 8))
+	fig.suptitle('channel low, noisy(top) vs denoised(bottom)', fontsize=30)
+	for j in range(n_pics):
+		ax1 = fig.add_subplot(1,n_pics,j+1)
+		ax1.get_xaxis().set_visible(False)
+		ax1.get_yaxis().set_visible(False)
+		ax1.set_title("image {}".format(j))
+		ax1.imshow(comp[j][0], interpolation=None, vmin=0.0, vmax=1.0, cmap='gray')
 
-    plt.savefig('n2s.png', dpi=500)
+	plt.savefig('n2s.png', dpi=500)
 
 
 if __name__=="__main__":
@@ -115,27 +118,31 @@ if __name__=="__main__":
 	# datasets
 	dataset = N2SDataset(args.data, sharp=True, **config.get("dataset", {}))
 
-	net_full_residual = torch.nn.Sequential(
+	simple_res_net = torch.nn.Sequential(
 		model.ConvBlock(1, 16, 3, padding_mode='reflect'),
-		model.ResBlock(16, 3, padding_mode='reflect', hidden_channels=[32, 32, 32]),
-		model.ResBlock(16, 3, padding_mode='reflect', hidden_channels=[32, 32, 32]),
-		model.OutConv(16, 1) 
-	)
-	net_res = torch.nn.Sequential(
-		# model.ConvBlock(1, 16, 3, padding_mode='reflect'),
 		model.ResBlock(1, 3, padding_mode='reflect', activation='relu', hidden_channels=[32, 32, 32]),
 		model.ResBlock(1, 3, padding_mode='reflect', activation='relu', hidden_channels=[32, 32, 32]),
-		#model.ConvBlock(1, 1, 3, padding_mode='reflect', activation='sigmoid'),
+		model.ConvBlock(1, 1, 3, padding_mode='reflect', activation='sigmoid'),
 	)
 
-	#from noise2self.models.babyunet import BabyUnet
-	#net_babyu = BabyUnet()
+	model_type = config['model']
+	if model_type == 'res':
+		from model import ResNet
+		net = ResNet(1, 1) # in, out channels
+	if model_type == 'babyu':
+		from noise2self.models.babyunet import BabyUnet
+		net = BabyUnet()
+	if model_type == 'unet':
+		from noise2self.models.unet import Unet
+		net = Unet()
+	if model_type == 'dncnn':
+		from noise2self.models.dncnn import DnCNN
+		net = DnCNN(16) # number of channels
+	if model_type == 'simple_res':
+		net = simple_res_net
 
-
-	if config['model'] == 'res':
-		net = net_res
 	net = net.float()
 
 	loss = MSELoss()
 
-	fit(net, loss, dataset, 10, batch_size=32)
+	fit(net, loss, dataset, 10, batch_size=32, device=args.device)
