a79923fe6dffba7ebf4ecfaa81e495383fe9352c
Jan <mail@janemrich.de>
2020-09-25 22:04:00 +0200

    apply demosaic for n2s-sharp

--- apply-demosaic.py
+++ apply-demosaic.py
@@ -2,7 +2,7 @@ import argparse
 import os
 import torch
 import numpy as np
-from data import ProDemosaicDataset
+from data import ProDemosaicDataset, SharpDemosaicDataset, N2SDataset
 import matplotlib.pyplot as plt
 from PIL import Image
 import json
@@ -43,8 +43,13 @@ if __name__=="__main__":
 	elif args.dataset == "sharp":
 		dataset = SharpDemosaicDataset(
 			args.inputdir,
+			crop=False)
+	elif args.dataset == "direct_sharp":
+		dataset = N2SDataset(
+			args.inputdir,
 			crop=False,
-			patches_per_image=1)
+			sharp=True,
+			patches_per_image=2)
 
 	state = torch.load(args.model, map_location=args.device)
 	if args.statedict:
